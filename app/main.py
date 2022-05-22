4b7c4fe02f8458cd9cbb9d967bf0b15f703a39fc
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2022-05-22 19:00:27 -0300

    ..

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-import base64
+import uuid
+from dataclasses import dataclass
 from io import BytesIO
 
 import face_recognition as fr
@@ -7,41 +8,65 @@ from fastapi import FastAPI, File, Form, Response, UploadFile, status
 from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 
-from app.firebase import db
-from app.util import optimize_img
+from firebase import db
+from util import optimize_img
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
+    allow_origins=['*'],
     allow_methods=["*"],
     allow_headers=["*"],
+    max_age=3600
 )
+
+
+@dataclass
+class UserData:
+    """Firebase user data"""
+    id: str
 
 
 @app.get('/')
 async def root():
-    return {'message': 'Api running...'}
+    return {'message': 'Api está funcionando...'}
 
 
-@app.post("/register", status_code=status.HTTP_201_CREATED)
+@app.post('/register', status_code=status.HTTP_201_CREATED)
 async def register(response: Response, name: str = Form(...), phone: str = Form(...), user_id: str = Form(...), file: UploadFile = File(...)):
     """
     Register new contact
     """
     img_bytes = await file.read()
     img_pillow = Image.open(BytesIO(img_bytes))
-    img_bytes = optimize_img(img_pillow)
-    img_optimized = Image.open(BytesIO(img_bytes))
-    unknown_img = np.asarray(img_optimized)
+    img_bytes_optimized = optimize_img(img_pillow)
+    img_pillow_optimized = Image.open(BytesIO(img_bytes_optimized))
+    unknown_img = np.asarray(img_pillow_optimized)
     if (fr.face_locations(unknown_img)):
-        contact_img = base64.b64encode(img_bytes)
-        data = {'name': name, 'phone': phone, 'image': str(contact_img)[
-            2:-1]}
+        data = {'id': str(uuid.uuid4()), 'name': name, 'phone': phone,
+                'image': img_bytes_optimized}
         db.collection('users').document(
             user_id).collection('contacts').add(data)
-        return {'message': f'Contato {name} adicionado com sucesso.'}
+        return {'message': f'{name} foi adicionado a lista de contatos'}
     response.status_code = status.HTTP_400_BAD_REQUEST
-    return {'message': f'Algo deu errado.'}
+    return {'message': f'Imagem inválida, tire outra foto.'}
+
+
+@app.post('/contacts')
+async def contacts(user: UserData):
+    """
+    List all contacts
+    """
+    contact_list = []
+    contacts = db.collection('users').document(
+        user.id).collection('contacts').get()
+    if contacts:
+        for contact in contacts:
+            current = contact.to_dict()
+            contact_data = {
+                'id': current['id'], 'name': current['name'], 'phone': current['phone']}
+            contact_list.append(contact_data)
+        return contact_list
+    return {'message': 'Erro ao listar contatos'}
